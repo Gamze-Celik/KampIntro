52bc9a244b4d1930c025e0c7fc083e93d8f6052f
gamze çelik <gamze.celik4@ogr.sakarya.edu.tr>
2022-06-15 16:38:12 +0300

    Homework

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> adYas = new MyDictionary<string, int>();
+            adYas.Add("Gözde", 20);
+            adYas.Add("Merve", 23);
+            adYas.Add("Ali", 25);
+
+            adYas.GetAll();
+
+        }
+    }
+}
+
